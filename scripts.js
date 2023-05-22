95d472d68200d01604bb2cb874c43b230ce0fafa
jessemayer1985@gmail <jessemayer1985@gmail.com>
2023-05-21 19:17:58 -0600

    getting tiles to work

--- scripts.js
+++ scripts.js
@@ -16,24 +16,57 @@ class Calculator {
     }
 
     appendNumber(number) {
-        this.currentOperand = number
+        if (number === "." && this.currentOperand.includes("."))return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
-    chooseOperations(operation) {
-
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== ""){
+            this.compute()
+        }
+      this.operation = operation
+      this.previousOperand = this.currentOperand
+      this.currentOperand = ''
     }
 
     compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN (prev) || isNaN(current)) return
+            switch (this.operation) {
+                case "+":
+                    computation = prev + current
+                    break;
+                case "-":
+                    computation = prev - current
+                    break;
+                case "*":
+                    computation = prev * current
+                    break;
+                case "/":
+                    computation = prev / current
+                    break;
+                default:
+                    break;
+            }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ""
 
     }
 
     updateDisplay() {
+       
         this.currentOperandTextElement.innerText = this.currentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
     }
 }
 
-const numberButtons = document.querySelectorAll("[data-number]")
-const operationButtons = document.querySelectorAll("[data-operation]")
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
@@ -48,3 +81,10 @@ numberButtons.forEach(button => {
         calculator.updateDisplay()
     })
 })
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.chooseOperation(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
